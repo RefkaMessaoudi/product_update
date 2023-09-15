3eba712731b1292690b97cc3c5e0c67dddcb30b7
refka <messaoudirefka22@gmail.com>
2023-09-15 12:15:54 +0200

    product_update

--- models/product_template.py
+++ models/product_template.py
@@ -8,11 +8,9 @@ class ProductTemplate(models.Model):
         res = super(ProductTemplate, self).write(vals)
         print('vals', vals)
         if vals:
-
-           product = self.env['product.update'].create({'product_update': vals,
-                                               'user_id': self.create_uid.id,
-                                               'date': self.create_date,
-                                               })
-           print('product', product.product_update)
+            product = self.env['product.update'].create({'product_update': vals,
+                                                         'user_id': self.create_uid.id,
+                                                         'date': self.create_date,
+                                                         })
+            print('product', product.product_update)
         return res
-
--- models/product_update.py
+++ models/product_update.py
@@ -4,6 +4,7 @@ from odoo import models, fields, api
 class ProductUpdate(models.Model):
     _name = 'product.update'
     _description = 'Product Update'
+    _rec_name= 'date'
 
     date = fields.Date('Date', tracking=True)
     user_id = fields.Many2one('res.users', 'Users', ondelete='cascade')
